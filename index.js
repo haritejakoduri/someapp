9140542569a40bc0db5f02aa94c92585223d14bb
haritejakoduri <haritejakoduri1234@gmail.com>
2021-03-23 18:22:30 +0530

    third commit

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ nsmarty.tpl_path = __dirname+'/template/';
 app.use(express.json());
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}));
-app.get("/",(req,res)=>{
-    nsmarty.clearCache('index.tpl');
-    var stream=nsmarty.assign('index.tpl');
+app.get('/',(req,res)=>{
+    res.redirect('/search')
+})
+app.get("/search",(req,res)=>{
+    nsmarty.clearCache('search.tpl');
+    var stream=nsmarty.assign('search.tpl');
     util.pump(stream,res);
 })
 app.listen(4000)
--- template/components/navbar.tpl
+++ template/components/navbar.tpl
@@ -1,5 +1,5 @@
 <nav class="nav justify-content-center">
   <a class="nav-link" href="#">Home</a>
-  <a class="nav-link" href="#">Search</a>
+  <a class="nav-link" href="/search">Search</a>
   <a class="nav-link" href="#">cart</a>
 </nav>
